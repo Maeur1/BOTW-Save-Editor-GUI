1037bcf2c8681b886baa9e8f04fdc0fd96258cf4
Mayur Panchal <panchal.mayur2@gmail.com>
2020-03-09 21:34:26 +1100

    Fix account mounting logic

--- source/mount.c
+++ source/mount.c
@@ -18,7 +18,6 @@ int mountSaveData()
 
     FsFileSystem tmpfs;
     AccountUid userID={0};
-    bool account_selected=0;
     u64 titleID=0x01007ef00011e000;
 
     rc = accountInitialize(AccountServiceType_Application);
@@ -29,26 +28,21 @@ int mountSaveData()
     }
 
     if (R_SUCCEEDED(rc)) {
-	rc = accountGetPreselectedUser(&userID);
+	    rc = accountGetPreselectedUser(&userID);
         accountExit();
 
-     if (R_FAILED(rc)) {
+        if (R_FAILED(rc)) {
             printf("(accountGetPreselectedUser) failed: 0x%x\n", rc);
             return 0;
         }
-     else if(!account_selected) {
-            printf("No user is currently selected.\n");
-            rc = -1;
-            return 0;
-        }
     }
 
     if (R_SUCCEEDED(rc)) {
-	printf("Using titleID=0x%016lx userID: 0x%lx 0x%lx\n", titleID, userID.uid[1], userID.uid[0]);
+	    printf("Using titleID=0x%016lx userID: 0x%lx 0x%lx\n", titleID, userID.uid[1], userID.uid[0]);
     }
 
     if (R_SUCCEEDED(rc)) {
-	rc = fsOpen_SaveData(&tmpfs, titleID, userID);
+	    rc = fsOpen_SaveData(&tmpfs, titleID, userID);
         if (R_FAILED(rc)) {
             printf("fsdevMountSaveData() failed: 0x%x\n", rc);
             return 0;
